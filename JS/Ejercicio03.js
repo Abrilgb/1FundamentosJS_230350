70421cf5f47f7c721e6d8c69dbeee88e3f8f4dc2
Abril Guzman Barerra <230350@utxicotepec.edu.mx>
2024-09-24 14:17:54 -0600

    Practica05
    Agrgacion de los apartados Actualizar, Modificacion, Agregacion, Eliminacion de objetos

--- JS/Ejercicio03.js
+++ JS/Ejercicio03.js
@@ -1,10 +1,11 @@
 //Repaso de objetos 
-const bg = "Liniear-gradient (11deg, rgba(2,0,36,1) 0%, rgba (9,9,121,1) 33%, rgba(0,212,255,1) 86%";
-const style_console= `background: ${bg}; color: white; 
-border radius: 6px; padding: 4px: font_size: 1.0rem; font_weight: bold`
+const bg ="linear-gradient(11deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 33%, rgba(0,212,255,1) 86%)";
+
+const style_console=`background:${bg}; color: white;
+border-radius: 6px; padding: 4px: font-size: 1.0rem; font-weight: bold`
 
 // Personalizacion de las salidas dfe consola 
-console.warn("Practica05 repaso de Objetos en JvaScript");
+console.warn("Practica 05 repaso de Objetos en JvaScript");
 
 //VARIABLES INDEPENDIENTES 
 console.log("%c1.- Variables Independientes", style_console);
@@ -51,7 +52,7 @@ let Producto =
     Precio: "3361.25",
     Disponibilidad:false,
     Stock: 0,
-    Imagen: "../aseets/products/sneakers/JORDAN.png",
+    Imagen:"../Aseets/products/sneakers/JORDAN.png",
     SKU: "DZ5485-612",
     Barcode:null,
     Categorias: ["Deportes", "Tenis", "Juvenil"]
@@ -72,7 +73,7 @@ else
 console.log(`Estatus: ${Producto_Stock} unidades disponibles`);
 
 
-console.log("%3.- Destructuracion de Objetos ", style_console);
+console.log("%c3.- Destructuracion de Objetos ", style_console);
 
 
 let Producto2 =
@@ -84,7 +85,7 @@ let Producto2 =
     Precio: "3361.25",
     Disponibilidad:true,
     Stock: 5,
-    Imagen: "../aseets/products/sungalsses/KatoRed.png",
+    Imagen: "../Aseets/products/sungalsses/KatoRed.png",
     SKU: "009481D-0356",
     Barcode:888392491626,
     Categorias: ["Deportes", "Lentes", "Hombre", "Accesorios"]
@@ -107,7 +108,8 @@ let Pedido=
      Producto_Clave: 316,
      Comprador_Clave: 3216,
      Cantidad: 2,
-     Estatus: "Carrito de Compra"
+     Estatus: "Carrito de Compra",
+     TipoPago:"Tarjeta de credito"
 
 }
 
@@ -119,5 +121,51 @@ let {SaldoActual:Cliente_SaldoActual }=Comprador;
 let Costo_Compra= Producto_Precio * Pedido_Cantidad;
 
 console.log(`El cliente ha agregado a su carritpo de compras ${Pedido_Cantidad} unidades, con un costo de: ${Costo_Compra}  `);
-if(Costo_Compra< Cliente_SaldoActual)
+if(Costo_Compra<Cliente_SaldoActual)
     console.log("El cliente tiene saldo suficinte");
+
+//Actualizar el valor de los Objetos 
+console.log("%c4.- Actualizacion de los valores de las propiedadezs de un objeto  ", style_console);
+console.log(`El objeto actualmente tiene las sigueientes valores`);
+console.log( JSON.stringify(Producto2, null, 2));
+//Convierte el objeto a una cadena para evitar problemad co la referencia 
+console.log(`Por cuestiones de inflacion el costo del producto ha cambiado y debeb ser a tuali9zado... de $6,829.00 a $6915.50`)
+
+//Parar modificar elm v alor de un objeto basta con igualar el nuevo valor de la propiedad deseada 
+
+Producto2.Precio=6915.50;
+console.log(`Los nuevos valores del producto son :`)
+console.log( Producto2);
+
+//¿Puedo cambiar no solo el valor si no el tipo de dato del un obejto en JS?
+console.log(`----------------------------------------`);
+console.log(`El objeto actuualemnte tiene los siguientes valores`);
+let= tipoDisponibilidad = typeof (Producto.Disponibilidad)
+console.log( `El tipo de dato de la disponibilñidad es : ${tipoDisponibilidad}`);
+console.log( JSON.stringify(Producto2, null, 2)); //Disponibilidad Booleano
+Producto2.Disponibilidad="Si";
+let nuevoTipoDisponibilidad = typeof(Producto2.Disponibilidad)
+console.log(Producto2)
+console.log(`El tipo de dato de la disponibilñidad es : ${nuevoTipoDisponibilidad}`)
+
+console.log("%c5.- Agrgar nuevas propiedades al obejto ", style_console);
+//Parar agregar una nueva propiedad utilizaremos el nombre del obejto de los corchetes [  ] y el nuevo de propiedad co s su valor por defecto
+console.log("Los datos actuales de un comprador son:")
+console.table(Comprador)
+Comprador[`Direccion`] = "Nicolas Bravo No. 131, Interiro C, Xicotepec de Juarez, Puebla, Mexio"
+Comprador[`Tipo`]="Nuevo cliente"
+Comprador[`ActividadReciente`]= true
+Comprador[`TotalCompras`]=3516.25
+console.log("Despues de haber agregado las propiedades Direccion, Tipo, ActividadReciente y TotalCompras....")
+console.table(Comprador)
+
+//Elegir propiedades existentes de un objeto 
+console.log("%c6.- Eliminar propiedades del objeto ", style_console);
+console.log("La estructura y valores del objeto PEDIDO son previos a la modificacion")
+console.table(Pedido)
+delete Pedido.TipoPago
+console.group("Despues de la modificacion ")
+console.table(Pedido)
+
+
+//
