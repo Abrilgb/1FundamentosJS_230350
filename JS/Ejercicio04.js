243c775be710475b50857e64bcb4176daca3a13e
Abril Guzmán barrera <230350@utxicotepec.edu.mx>
2024-10-10 23:49:11 -0600

    Practica04
    Se corrigieron errores de los primeros apartados del ejercicio

--- JS/Ejercicio04.js
+++ JS/Ejercicio04.js
@@ -22,22 +22,78 @@ console.table(mesesAnio)
 
 console.log(`El mes del año en la posicion -1 es:  ${mesesAnio[-1]}`)
 console.log(`El terceeavo mes del año en la posicion es:  ${mesesAnio[12]}`)
-console.log(`¿Que tuipo de dato es un arreglo?  ${typeof(mesesAnio)}`)
+console.log(`¿Que tipo de dato es un arreglo?  ${typeof(mesesAnio)}`)
 
-let arregloMixto
+const saludar =  function(nombreUsuario){return `Hola, ${nombreUsuario}!`}
 
+//Y que tipo de dato tiene esta constante
+console.log(`El tipo de dato de la constante saludar es: ${typeof(saludar)}`)
+let arreglomixto=["String",5,86.25,-200,-.1586,Symbol("March"), 'Z', false,null, BigInt(6454444444444444444444444444444444146),true,{latitud:"20° 18' 0\" N" , longitud:"97° 58' 00\" W",altitud:796},saludar]
 
+console.log(arreglomixto);
+console.table(arreglomixto);
+console.log(typeof(arreglomixto));
 
-
-
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[0] = ${arreglomixto[0]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[1] = ${arreglomixto[1]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[2] = ${arreglomixto[2]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[3] = ${arreglomixto[3]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[4] = ${arreglomixto[4]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[5] = ${arreglomixto[5].toString} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[6] = ${arreglomixto[6]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[7] = ${arreglomixto[7]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[8] = ${arreglomixto[8]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[9] = ${arreglomixto[9]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[10] = ${arreglomixto[10]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[11] = ${arreglomixto[11]} y es del tipo: ${(typeof(arreglomixto))}`)
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+console.log(`El dato en la posicion[12] = ${arreglomixto[12]} y es del tipo: ${(typeof(arreglomixto))}`)
 
 //Funciones y metodos de Arreglos 
 //Los metodos de un objeto siempre son invocados usamdo un . y al termino  se delimitan los para,etros entre (), en caso de que no lleve (), no es un metodo si no una propiedad 
 console.log("%c3.- Funciones y etodos de los Arreglos(Array Methos or Array Functions) ", style_console);
 
+let matriz= [[1,2,3,4],['a','b','c','d']];
+// esta es una matriz regular de 4 x 4
+
+console.log(matriz);
+console.table(matriz);
+
+console.log("Declarando una matriz irregular");
+
+const matrizIrregular=[["Juan","Pedro","Maria","Inés"],[true,false,null],[9.2]]
+
+console.log(matrizIrregular);
+console.table(matrizIrregular);
+
+// Accediendo a los valores de una matriz
+
+console.log("Leyendo las Propiedades de una Matriz Regular")
+console.log(`Elemento en la posición [0][0]: ${matriz[0][0]}`)
+console.log(`Elemento en la posición [1][2]: ${matriz[1][2]}`)
+
+//Accediendo a los valors de una matriz irregular
+console.log("Leyendo las Propiedades de una Matriz irregular")
+console.log(`Elemento en la posición [0][0]: ${matrizIrregular[0][2]}`)
+console.log(`Elemento en la posición [2][0]: ${matrizIrregular[2][0]}`)
+console.log(`Elemento en la posición [1][2](¿Es Maria mayor de edad?): ${matrizIrregular[1][2]}`)
+
+
 console.log("Como saber cual es el tamaño del arreglo?")
 console.log(`meses es un arreglo de tamaño: ${mesesAnio.length}`)
-console.log(`arregloMixto es un arreglo de tamaño: ${arregloMixto}`)
+//console.log(`arregloMixto es un arreglo de tamaño: ${arreglomixto}`);
 console.log(`Que pasa con los multidimensionales `)
 console.log(`matriz de un arreglo de tamaño: ${matriz.length}`)
 console.log("mmmm, eso me da el tamaño de numero de filas de la matriz , pero como saber el numero de columnas ?")
@@ -63,13 +119,13 @@ console.log("Despus de agregar los elementos del arreglo son: ")
 console.table(estudiantes)
 console.log("Que pasa con los Mixtos ?")
 console.log("El ArregloMixto actualamnete tiene los siguientes elementos")
-console.table(arregloMixto)
+console.table(arreglomixto)
 console.log("Agregamos la palabra : \"Hola\", como nuevo elemto")
-arregloMixto.push("Hola")
+arreglomixto.push("Hola")
 console.log("Yb tambien agregamos el numero 215599484663.2566145898, siendo este un BigInt ")
-arregloMixto.push((-215599484663.2566145898));
+arreglomixto.push((-215599484663.2566145898));
 console.log("Despues de estas operciones el arreglo queda cn los siguintes elementos: ")
-console.table(arregloMixto)
+console.table(arreglomixto)
 
 console.log("%c5.- Agregar un nuevo elemento a un arreglo (UNSHIFT) EN LA POSICICON INICIAL ", style_console);
 console.table(estudiantes)
@@ -82,7 +138,7 @@ console.log("%c7.- Eliminar el elemento de un arreglo en la ultima pocicion (POP
 console.log("El arreglo tiene los siguientes elementos: ")
 console.table(estudiantes)
 console.log("Para este caso eliminaremos a Abril Guzman en la ultima opcion ")
-console.pop()
+estudiantes.pop()
 console.log("Despus de eliminar el ultimo elemento el arreglon quedo de la siguiente manera  ")
 console.table(estudiantes);
 
@@ -142,7 +198,7 @@ console.log(`El septimo signo zodiacal es: ${signo7}`)
 //console.log(`El cuarto signo zodiacal es: ${signo4}`)
 
 //Congelamos el arreglo volviendo INMUTABLE 
-Objeto.freeze(signosZodiacales);
+Object.freeze(signosZodiacales);
 
 //Filtrado de datos 
 console.log("%c11.- Fitrado de elemto dentro de un arreglo (FILTER) ", style_console);
@@ -159,13 +215,16 @@ console.table(estudiantes);
 
 Object.freeze(estudiantes)
 //es un metodo que recorre los elemetos de un arreglo haciendo alguna tarea en especifico, lo que temos que considerara es que este nuevo arreglo resultante e su objeto muevo que puede ser mutable
-console.log("Filtrando los primeros cinco elemetos");
-let nuevoEstudiantes = estudientes.filter(estudiantes, num => num>5);
-console.table(nuevoEstudiantes) 
+console.log("Filtrando los primeros 5 elementos")
+let nuevoEstudiantes= estudiantes.filter((estudiante,index)=> index<5);
+console.log("Prueba 1")
+console.table(nuevoEstudiantes);
+console.log("Prueba 2")
+console.table(filtrarprimeros5(estudiantes))
 
 //filtarar a los estudinates que su nombre tenga mas de 15 caracteres 
-let nuevoEstudiantes1 = estudientes.filter(estudiantes,index (index => 10));
-console.table(nuevoEstudiantes) 
+let nuevoEstudiantesNombre= estudiantes.filter((estudiante)=> estudiante.length>15);
+console.table(nuevoEstudiantesNombre)
 
 
 
@@ -191,18 +250,18 @@ function filtrarprimeros5(estudiantes){
 
 console.log("%c12.- Filtrado de elementos de un arreglo utilizando el mertdodo map en el que necesitamos transformarlos ", style_console);
 console.log("Imprimirmos los elementos acatuales de Zignos sodiacales ")
-consolew.table(signosZodiacales);
+console.table(signosZodiacales);
 
 //
 
 console.table(signosZodiacales.map(signoZodiacal=>signoZodiacal.toUpperCase()));
 //Redujiendo eleemtos de un arreglo, se usa cuando se debe hacer operaciones automaticas o cuantitaticvas a aun arreglo, como optener totales, reducir la lista mas simplificada 
 
-const costosListaCompra=[15,52,16.90, 32.50,28,105,45.294,10]
-//Como podemos calcular un lista de costos de un carritp de cp,pras 
-console.log("Los precios son : ");
-console.table(costosListaCompra);
-console.log(`El total de la compra es: ${costosListaCompra.reduce(total,precio=> total,precio, 0 ).toFixed(2)}`);
+const costosListaCompras=[15,52.50,16.90,32.50,28,105,45.2,94.10]
+//Cómo podemos calcular el total de una lista de costos de un carrito de compras 
+console.log("Los precios son:")
+console.table(costosListaCompras)
+console.log(`El total de la compra es: ${costosListaCompras.reduce((total,precio)=>total+precio,0).toFixed(2)}`)
 
 
 
